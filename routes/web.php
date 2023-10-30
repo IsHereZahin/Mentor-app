57d48e76eb3f57a5e5cbc2932b986a35a2c05ee7
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2023-10-30 20:44:33 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Website\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+# --------------------------------------- website -------------------------------------------------------
+Route::get('/',[HomeController::class,'index'])->name('website.home');
+
+
+# --------------------------------------- admin panel -------------------------------------------------------
+Route::middleware(['auth', 'verified'])->group(function () {
+    # dashboard
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+});
+
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
