964776256171fa66194b7c4931a63fbd18ebb7c9
DeveloperZahin <abuzahinmohammadnowsin@gmail.com>
2023-11-20 19:40:26 +0600

    My Profile Linkup

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -188,8 +188,9 @@
                             <img src="../../assets/images/users/1.jpg" alt="user" class="rounded-circle" width="31">
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="javascript:void(0)"><i class="ti-user me-1 ms-1"></i>
-                                My Profile</a>
+                            <a class="dropdown-item" href="{{ route('profile.edit') }}" target="blank" ><i class="ti-user me-1 ms-1"></i>
+                                My Profile
+                            </a>
                             <div class="dropdown-divider"></div>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="{{ route('logout') }}"><i class="fa fa-power-off me-1 ms-1"></i> Logout</a>
